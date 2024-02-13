56085732b0865d27cab337b472939f0ee714ef22
Sofia Bezertzi <sofia_bezertzi@waters.com>
2024-02-13 14:20:28 +0000

    add tests for the property details

---
+++ src/pages/details/PropertyDetails.spec.tsx
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { SINGLE_PROPERTY } from 'src/__mocks__/fixtures/property/property';
+import PropertyDetails from './PropertyDetails';
+
+vi.mock('react-query', () => ({
+  useQuery: () => ({
+    data: SINGLE_PROPERTY('1'),
+    isLoading: false,
+  }),
+}));
+
+describe('Property Details Page', () => {
+  it('it should render with short description', () => {
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/property/1']}>
+        <Routes>
+          <Route path="/property/:id" element={<PropertyDetails />} />
+        </Routes>
+      </MemoryRouter>,
+    );
+    expect(getByTestId('property-desc')).toHaveTextContent('Two bedroom house in LA');
+  });
+
+  it('it should render with long description', () => {
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/property/1']}>
+        <Routes>
+          <Route path="/property/:id" element={<PropertyDetails />} />
+        </Routes>
+      </MemoryRouter>,
+    );
+    expect(getByTestId('property-desc')).toHaveTextContent(SINGLE_PROPERTY('1').description);
+  });
+});
